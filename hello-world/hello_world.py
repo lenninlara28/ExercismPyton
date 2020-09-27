d9082d0cdcc0cd25a0ee6560adfa9fbe2f3d1788
LenninLara <lenninlara28@hotmail.com>
2020-09-27 18:32:12 -0500

    Inicio de Ejercicios

---
+++ hello-world/hello_world.py
@@ -0,0 +1,8 @@
+def hello():
+    """
+    Este Metodo Saluda al mundo
+
+    Returns
+    Saludo
+    """
+    return "Hello, World!"
---
+++ two-fer/two_fer.py
@@ -0,0 +1,13 @@
+def two_fer(name="you"):
+    """
+    Este Metodo regresa una oración con un nombre igresado
+
+
+    Parameters
+    ----------
+    name = nombre ingresado
+
+    Returns
+    retorna la oración concatenada con el nombre ingresado
+    """
+    return f'One for {name}, one for me.'
