c97dce2e7760f404bd076224ccf183257cb9e843
LenninLara <lenninlara28@hotmail.com>
2020-09-27 19:05:44 -0500

    Ejercicio Raindrops Resuelto

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,2 +1,23 @@
-def convert(number):
-    pass
+def convert(number, result=""):
+    """
+    Este Metodo convierte un número en una cadena que contenga sonidos de gotas
+    de lluvia correspondientes a ciertos factores potenciales.
+
+
+    Parameters
+    ----------
+    number = numero a convertir.
+
+    Returns
+    Retorna Una Cadena de PlingPlongPlang o en su caso el mismo numero
+    """
+
+    if number % 3 == 0:
+        result = result + "Pling"
+    if number % 5 == 0:
+        result = result + "Plang"
+    if number % 7 == 0:
+        result = result + "Plong"
+    if number % 3 != 0 and number % 5 != 0 and number % 7 != 0:
+        result = f"{number}"
+    return result
